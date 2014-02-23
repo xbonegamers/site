9616dc2ab3adfcd0aba04bc8681c7d0f3a8cc743
Kevin Western <kevinwestern@gmail.com>
2014-02-23 09:44:46 -0800

    Filtering now working

--- public/js/app.js
+++ public/js/app.js
@@ -23593,23 +23593,81 @@ app.filter('decode', ['$window', function($window) {
   };
 }]);
 
-app.controller('GamersCtrl', ['$http', function($http) {
-  this.gamers = [];
+app.controller('GamersCtrl', ['$scope', '$http', function($scope, $http) {
+  this.gamerSets = [];
+  this.games = [{
+    selected: false,
+    label: 'Battlefield 4'
+  }, {
+    selected: false,
+    label: 'Call of Duty: Ghosts'
+  }, {
+    selected: false,
+    label: 'Need For Speed'
+  }, {
+    selected: false,
+    label: 'Titanfall'
+  }];
   this.gamerTag = '';
-  var self = this;
+
+  var gameToGamers = {};
+  var gamerTagToGamer = {};
+  var nonFilteredGamers = [];
+
   var refresh = function() {    
     $http.get('/gamers').success(function(gamers) {
-      self.gamers = gamers;
-      for (var i = 0; i < 22; i++) {
-        self.gamers.push(angular.copy(self.gamers[i]));
-      }
-    });
-  };
+      gamers.forEach(function(gamer) {
+        gamerTagToGamer[gamer.gamerTag] = gamer;
+        gamer.games.forEach(function(game) {
+          if (!gameToGamers[game]) {
+            gameToGamers[game] = [];
+          }
+          gameToGamers[game].push(gamer);
+        });
+      });
+
+      nonFilteredGamers = gamers;
+      this.gamerSets = this.groupGamers(angular.copy(gamers));
+    }.bind(this));
+  }.bind(this);
 
   this.add = function() {
     $http.post('/gamers', {gamerTag: self.gamerTag}).success(function(gamer, status) {
       refresh();
     });
   };
+
+  this.groupGamers = function(gamers) {
+    var result = [];
+    for (var i = 0; i < gamers.length; i += 3) {
+      result.push(gamers.slice(i, i + 3));
+    }
+    return result;
+  };
+
+  $scope.$watch(function() {
+    return this.games;
+  }.bind(this), function() {
+    var gamerTags = {};
+    var noneSelected = true;
+    this.games.forEach(function(game) {
+      if (game.selected) {
+        noneSelected = false;
+        gameToGamers[game.label].forEach(function(gamer) {
+          if (!gamerTags[gamer.gamerTag]) {
+            gamerTags[gamer.gamerTag] = true;
+          }
+        });
+      }
+    });
+    if (noneSelected) {
+      this.gamerSets = this.groupGamers(nonFilteredGamers);
+    } else {
+      this.gamerSets = this.groupGamers(Object.keys(gamerTags).map(function(gamerTag) {
+        return gamerTagToGamer[gamerTag];
+      }));
+    }
+  }.bind(this), true);
+
   refresh();
 }]);
