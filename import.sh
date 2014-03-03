85da14fb470d3696c5511fec6aebe3d0bb3268d4
Kevin Western <kevinwestern@gmail.com>
2014-03-02 20:35:06 -0800

    updated import

--- import.sh
+++ import.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-mongo gamerfriends --eval "db.gamers.remove()";
+mongo gamerfriends --eval "db.gamers.remove()"
+mongo gamerfriends --eval "db.games.remove()"
 mongoimport --db gamerfriends --collection gamers --file gamers.json --jsonArray
 mongoimport --db gamerfriends --collection games --file games.json --jsonArray
